69c3dd373f0a913bd5ac88e63ea07bde06d78f30
符其军 <fuqijun@mapledeMacBook-Pro.local>
2019-11-09 12:38:50 +0800

    aggregate turbine

--- turbine/src/main/java/com/soa/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/soa/turbine/TurbineApplication.java
@@ -2,8 +2,12 @@ package com.soa.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
 public class TurbineApplication {
 
     public static void main(String[] args) {
